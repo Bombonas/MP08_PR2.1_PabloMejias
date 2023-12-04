8b54a4d8f7313453415c4c8acd865e26f64266c0
Bombonas <pablomejiast@gmail.com>
2023-12-04 17:28:37 +0100

    commit furula

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.pr21_pablomejias"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.pr21_pablomejias"
